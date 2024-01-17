9b158ef52e29eefed90ecf2414c2e311f4c2da9f
sergeyabobrov <bobrovsa@rambler.ru>
2024-01-17 11:18:55 -0500

    autocommit

---
+++ gameback/settings/production.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['games.bobrobotirk.ru']
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
