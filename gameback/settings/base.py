cd4ee8a2fa75909e9cdc9cba6f9be751433c9bc8
sergeyabobrov <bobrovsa@rambler.ru>
2024-01-18 12:54:59 -0500

    -hijack

--- gameback/settings/base.py
+++ gameback/settings/base.py
@@ -23,7 +23,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'gameback.urls'
