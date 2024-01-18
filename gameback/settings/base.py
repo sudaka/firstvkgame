0e346ea6fe3b0ee55bacf6e2dc1dba75a27d2835
sergeyabobrov <bobrovsa@rambler.ru>
2024-01-18 13:10:15 -0500

    we

--- gameback/settings/base.py
+++ gameback/settings/base.py
@@ -79,7 +79,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'assets/'
+STATIC_URL = '/assets/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'tmpassets')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
