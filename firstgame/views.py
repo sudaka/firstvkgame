032918dcf5848ca5476fca0ead31a10d449fdad4
sergeyabobrov <bobrovsa@rambler.ru>
2024-01-20 06:48:25 -0500

    -dict

--- firstgame/views.py
+++ firstgame/views.py
@@ -3,7 +3,7 @@ from django.conf import settings as cur_settings
 from gameback.vk_common import checksign
 
 def index(request):
-    curget = str(request.GET)
+    curget = dict(request.GET)
     pkey = cur_settings.VK_PRIVATE_KEY
     (checkresult, state) = checksign(pkey, curget)
     return render(request, 'index.html', {"getparam": state})
