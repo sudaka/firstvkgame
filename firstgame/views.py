a2375711db5150172c03632df0a1d27d08f9c323
sergeyabobrov <bobrovsa@rambler.ru>
2024-01-20 06:32:58 -0500

    +sign

--- firstgame/views.py
+++ firstgame/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.conf import settings as cur_settings
+from gameback.vk_common import checksign
 
 def index(request):
     curget = str(request.GET)
-    return render(request, 'index.html', {"getparam": curget})
+    pkey = cur_settings.VK_PRIVATE_KEY
+    (checkresult, state) = checksign(pkey, curget)
+    return render(request, 'index.html', {"getparam": checkresult})
 
---
+++ gameback/vk_common.py
@@ -0,0 +1,32 @@
+from urllib import parse
+from hashlib import sha256
+import hmac
+import base64
+
+def checksign(p_key, data):
+    error = False
+    if isinstance(data, dict):
+        skeys = data.get('sign_keys','')
+        skeys_list = str.split(skeys, ',')
+        signed_data = {}
+        for key in skeys_list:
+            signed_data[key] = data.get(key, '')
+        forsignstr = parse.urlencode(signed_data)
+        raw = forsignstr.encode("utf-8")
+        curkey = p_key.encode("utf-8")
+        hashed = hmac.new(curkey, raw, sha256)
+        strhash = base64.encodebytes(hashed.digest()).decode('utf-8')
+        without_last = strhash.rstrip('\n').rstrip('=')
+        rep_arr = {
+            '+': '-',
+            '/': '_'
+        }
+        for orig, repr in rep_arr.items():
+            without_last = without_last.replace(orig, repr)
+        cur_sign = data.get('sign', 'Error')
+        if cur_sign == without_last:
+            return(True, 'Ok')
+        else:
+            return(False,'Bad sign')
+    else:
+        return(False, 'Error CheckSign_1: Data not a dict')
